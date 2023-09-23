2732c1762e3d53f6ca52d172220c62d4135d7eb0
hyeri00 <nexusring@gmail.com>
2023-09-23 23:09:00 +0900

    loginViewController buttonAction 수정하기

--- Hoegaarden/UI/Login/LoginViewController.swift
+++ Hoegaarden/UI/Login/LoginViewController.swift
@@ -18,58 +18,61 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupAddTarget()
+        googleLoginResult()
+        kakaoLoginResult()
+        appleLoginResult()
     }
     
-    private func setupAddTarget() {
-        loginView.googleStartButton.addTarget(self, action: #selector(googleLoginStartButton), for: .touchUpInside)
-        loginView.kakaoStartButton.addTarget(self, action: #selector(kakaoLoginStartButton), for: .touchUpInside)
-        loginView.appleStartButton.addTarget(self, action: #selector(appleLoginStartButton), for: .touchUpInside)
+    private func googleLoginResult() {
+        loginView.googleStartButton.addAction(UIAction(handler: { _ in
+//            GoogleLoginManager.shared.signIn(withPresenting: self)
+            
+            let nextVC = HomeViewController()
+            nextVC.modalPresentationStyle = .fullScreen
+            let nav = UINavigationController(rootViewController: nextVC)
+            nav.modalPresentationStyle = .overFullScreen
+            self.present(nav, animated: true, completion: nil)
+        }), for: .touchUpInside)
     }
     
-    @objc private func googleLoginStartButton() {
-//        GoogleLoginManager.shared.signIn(withPresenting: self)
-        let nextVC = HomeViewController()
-        nextVC.modalPresentationStyle = .fullScreen
-        let nav = UINavigationController(rootViewController: nextVC)
-        nav.modalPresentationStyle = .overFullScreen
-        self.present(nav, animated: true, completion: nil)
-    }
-        
-    @objc private func kakaoLoginStartButton() {
-        KakaoLoginManager.loginWithKakao(with: self) { result in
-            switch result {
-            case .success(let userId):
-                print("Kakao login success with user id: \(userId)")
-                
-                let nextVC = HomeViewController()
-                nextVC.modalPresentationStyle = .fullScreen
-                let nav = UINavigationController(rootViewController: nextVC)
-                nav.modalPresentationStyle = .overFullScreen
-                self.present(nav, animated: true, completion: nil)
-                
-            case .failure(let error):
-                print("Kakao login failed with error: \(error)")
+    private func kakaoLoginResult() {
+        loginView.kakaoStartButton.addAction(UIAction(handler: { _ in
+            
+            KakaoLoginManager.loginWithKakao(with: self) { result in
+                switch result {
+                case .success(let result):
+                    
+                    let nextVC = HomeViewController()
+                    nextVC.modalPresentationStyle = .fullScreen
+                    let nav = UINavigationController(rootViewController: nextVC)
+                    nav.modalPresentationStyle = .overFullScreen
+                    self.present(nav, animated: true, completion: nil)
+                    
+                case .failure(let error):
+                    print("Kakao login failed with error: \(error)")
+                }
             }
-        }
+        }), for: .touchUpInside)
     }
     
-    @objc private func appleLoginStartButton() {
-        AppleLoginManager.shared.startAppleLogin { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case .success(let userId):
-                print("Apple login success with user id: \(userId)")
-                
-                let nextVC = HomeViewController()
-                nextVC.modalPresentationStyle = .fullScreen
-                let nav = UINavigationController(rootViewController: nextVC)
-                nav.modalPresentationStyle = .overFullScreen
-                self.present(nav, animated: true, completion: nil)
-                
-            case .failure(let error):
-                print("Apple login failed with error: \(error)")
+    private func appleLoginResult() {
+        loginView.appleStartButton.addAction(UIAction(handler: { _ in
+            
+            AppleLoginManager.shared.startAppleLogin { [weak self] result in
+                guard let self = self else { return }
+                switch result {
+                case .success(let result):
+                    
+                    let nextVC = HomeViewController()
+                    nextVC.modalPresentationStyle = .fullScreen
+                    let nav = UINavigationController(rootViewController: nextVC)
+                    nav.modalPresentationStyle = .overFullScreen
+                    self.present(nav, animated: true, completion: nil)
+                    
+                case .failure(let error):
+                    print("Apple login failed with error: \(error)")
+                }
             }
-        }
+        }), for: .touchUpInside)
     }
 }
